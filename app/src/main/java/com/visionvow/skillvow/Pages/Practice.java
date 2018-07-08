36da88ba1cff4847d84d0c08b62b81d30afa6a8c
Jason Li <visionvow@gmail.com>
2018-07-08 17:22:53 -0400

    Added tabs at the top of the page

---
+++ app/src/main/java/com/visionvow/skillvow/Pages/Practice.java
@@ -0,0 +1,24 @@
+package com.visionvow.skillvow.Pages;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.visionvow.skillvow.R;
+
+public class Practice extends Fragment {
+    View view;
+
+    public Practice() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.practice_fragment, container, false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/visionvow/skillvow/Pages/Progress.java
@@ -0,0 +1,24 @@
+package com.visionvow.skillvow.Pages;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.visionvow.skillvow.R;
+
+public class Progress extends Fragment {
+    View view;
+
+    public Progress() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.progress_fragment, container, false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
