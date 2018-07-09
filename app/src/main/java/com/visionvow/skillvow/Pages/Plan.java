939eca5a46b76a5f4f83a35ddbb5fdc069451529
Jason Li <visionvow@gmail.com>
2018-07-09 15:30:51 -0400

    Activity now switches when you click the add button in plan

--- app/src/main/java/com/visionvow/skillvow/Pages/Plan.java
+++ app/src/main/java/com/visionvow/skillvow/Pages/Plan.java
@@ -1,12 +1,16 @@
 package com.visionvow.skillvow.Pages;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.visionvow.skillvow.PlanActivity;
 import com.visionvow.skillvow.R;
 
 public class Plan extends Fragment {
@@ -19,6 +23,32 @@ public class Plan extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.plan_fragment, container, false);
+
+        //Sets up add button
+        FloatingActionButton addButton = view.findViewById(R.id.plus_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), PlanActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
+
+//    @Override
+//    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+//        super.onActivityCreated(savedInstanceState);
+//
+//        //Sets up add button
+//        FloatingActionButton addButton = view.findViewById(R.id.plus_button);
+//        addButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getActivity(), PlanActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//    }
 }
---
+++ app/src/main/java/com/visionvow/skillvow/PlanActivity.java
@@ -0,0 +1,14 @@
+package com.visionvow.skillvow;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class PlanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_plan_activity);
+    }
+}
